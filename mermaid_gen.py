b80f4180dbc31ccd507c1723e3c5add5316cbba5
pktiuk <kotiuk@wp.pl>
2020-07-11 23:45:08 +0200

    Add loops for fields and methods

--- mermaid_gen.py
+++ mermaid_gen.py
@@ -3,22 +3,37 @@
 import string
 import CppHeaderParser
 import sys
+import string
+
+
+output = "classDiagram\n"
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 0:
+    if len(sys.argv) <= 1:
         sys.exit(-1)
-    
+
+    sys.argv.pop(0)  # remove script name
+
     for header_file in sys.argv:
+        print("Parsing file: " + header_file)
         try:
-            cppHeader = CppHeaderParser.CppHeader("SampleClass.h")
+            cppHeader = CppHeaderParser.CppHeader(header_file)
         except CppHeaderParser.CppParseError as e:
             print(e)
             sys.exit(1)
+        for className, classFile in cppHeader.classes.items():
+            print(className)
+            output += "\tclass "+className+"{\n"
 
-        print("Parsing file: "+ header_file)
-        
+            for method in classFile["methods"]["public"]:
+                output += "\t\t+" + method["name"] + "\n"
+            for method in classFile["methods"]["private"]:
+                output += "\t\t-" + method["name"] + "\n"
 
-
+            for field in classFile["properties"]["private"]:
+                output += "\t\t-" + field["name"] + "\n"
+            for field in classFile["properties"]["public"]:
+                output += "\t\t+" + field["name"] + "\n"
 
     pass
