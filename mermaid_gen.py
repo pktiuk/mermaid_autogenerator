e463aaf2714c310c142e4eb96627f427d9c899e5
pktiuk <kotiuk@wp.pl>
2020-07-12 00:32:25 +0200

    Create class cpp_graph

--- mermaid_gen.py
+++ mermaid_gen.py
@@ -6,7 +6,37 @@ import sys
 import string
 
 
-output = "classDiagram\n"
+class cpp_graph:
+    def __init__(self, header_file):
+        self.cppHeader = CppHeaderParser.CppHeader(header_file)
+
+        self.output = "classDiagram\n"
+        self.PRIVATE_SIGN = '-'
+        self.PUBLIC_SIGN = '+'
+
+    def generateClassDiagram(self):
+        for className, classFile in self.cppHeader.classes.items():
+            print(className)
+            self.output += "\tclass "+className+"{\n"
+
+            for method in classFile["methods"]["public"]:
+                self.loadMethod(method)
+            for method in classFile["methods"]["private"]:
+                self.loadMethod(method, False)
+
+            for field in classFile["properties"]["private"]:
+                self.output += "\t\t-" + field["name"] + "\n"
+            for field in classFile["properties"]["public"]:
+                self.output += "\t\t+" + field["name"] + "\n"
+
+    def loadMethod(self, method, is_public: bool = True):
+        if is_public:
+            self.output += "\t\t+" + method["name"] + "\n"
+        else:
+            self.output += "\t\t-" + method["name"] + "\n"
+
+        pass
+    pass
 
 
 if __name__ == "__main__":
@@ -18,22 +48,12 @@ if __name__ == "__main__":
     for header_file in sys.argv:
         print("Parsing file: " + header_file)
         try:
-            cppHeader = CppHeaderParser.CppHeader(header_file)
+            graph = cpp_graph(header_file)
         except CppHeaderParser.CppParseError as e:
             print(e)
             sys.exit(1)
-        for className, classFile in cppHeader.classes.items():
-            print(className)
-            output += "\tclass "+className+"{\n"
 
-            for method in classFile["methods"]["public"]:
-                output += "\t\t+" + method["name"] + "\n"
-            for method in classFile["methods"]["private"]:
-                output += "\t\t-" + method["name"] + "\n"
-
-            for field in classFile["properties"]["private"]:
-                output += "\t\t-" + field["name"] + "\n"
-            for field in classFile["properties"]["public"]:
-                output += "\t\t+" + field["name"] + "\n"
+        graph.generateClassDiagram()
+        print(graph.output)
 
     pass
