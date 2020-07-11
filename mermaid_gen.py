345d58074fd0707e0de7f780e8f597e9af24c0f2
pktiuk <kotiuk@wp.pl>
2020-07-11 21:22:42 +0200

    Add basic loop

--- mermaid_gen.py
+++ mermaid_gen.py
@@ -2,8 +2,22 @@
 
 import string
 import CppHeaderParser
+import sys
+
 
 if __name__ == "__main__":
+    if len(sys.argv) == 0:
+        sys.exit(-1)
+    
+    for header_file in sys.argv:
+        try:
+            cppHeader = CppHeaderParser.CppHeader("SampleClass.h")
+        except CppHeaderParser.CppParseError as e:
+            print(e)
+            sys.exit(1)
+
+        print("Parsing file: "+ header_file)
+        
 
 
 
