c5970ecd824baa25eab9059a63bd1d29897f4665
pktiuk <kotiuk@wp.pl>
2020-07-12 23:30:58 +0200

    Finish basic class printing

--- mermaid_gen.py
+++ mermaid_gen.py
@@ -24,10 +24,12 @@ class cpp_graph:
             for method in classFile["methods"]["private"]:
                 self.loadMethod(method, False)
 
-            for field in classFile["properties"]["private"]:
-                self.output += "\t\t-" + field["name"] + "\n"
-            for field in classFile["properties"]["public"]:
-                self.output += "\t\t+" + field["name"] + "\n"
+            for property in classFile["properties"]["public"]:
+                self.loadProperty(property)
+            for property in classFile["properties"]["private"]:
+                self.loadProperty(property, False)
+
+            self.output += "\t}\n\n"
 
     def loadMethod(self, method, is_public: bool = True):
         if is_public:
@@ -35,7 +37,12 @@ class cpp_graph:
         else:
             self.output += "\t\t-" + method["name"] + "\n"
 
-        pass
+    def loadProperty(self, property, is_public: bool = True):
+        if is_public:
+            self.output += "\t\t+" + property["name"] + "\n"
+        else:
+            self.output += "\t\t-" + property["name"] + "\n"
+
     pass
 
 
