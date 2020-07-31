4d9b62e5cc8e9f6bc8c40dc52420b904cadf5dc1
pktiuk <kotiuk@wp.pl>
2020-07-31 18:54:09 +0200

    Change generating output string

--- mermaid_gen.py
+++ mermaid_gen.py
@@ -9,7 +9,8 @@ import string
 class CppGraph:
     def __init__(self):
         # output string
-        self.output = "classDiagram\n"
+        self.class_hierarhy_str = str()
+        self.class_descriptions = str()
         # pre-defined signs and symbols
         self.PRIVATE_SIGN = '-'
         self.PUBLIC_SIGN = '+'
@@ -23,7 +24,7 @@ class CppGraph:
 
     def generateClassDiagram(self):
         for className, classFile in self.all_of_classes.items():
-            self.output += "\tclass "+className+"{\n"
+            self.class_descriptions += "\tclass "+className+"{\n"
 
             for method in classFile["methods"]["public"]:
                 self.loadMethod(method)
@@ -35,31 +36,34 @@ class CppGraph:
             for property in classFile["properties"]["private"]:
                 self.loadProperty(property, False)
 
-            self.output += "\t}\n\n"
+            self.class_descriptions += "\t}\n\n"
 
     def loadMethod(self, method, is_public: bool = True):
         if is_public:
-            self.output += "\t\t+" + method["name"]
+            self.class_descriptions += "\t\t+" + method["name"]
         else:
-            self.output += "\t\t-" + method["name"]
-        self.output += "("
+            self.class_descriptions += "\t\t-" + method["name"]
+        self.class_descriptions += "("
         for param in method["parameters"]:
-            self.output += param["name"] + ", "
+            self.class_descriptions += param["name"] + ", "
         # remove excessive ", " if they were added
         if len(method["parameters"]) != 0:
-            self.output = self.output[0:-2]
+            self.class_descriptions = self.class_descriptions[0:-2]
 
-        self.output += ")"
+        self.class_descriptions += ")"
 
         if method["returns"].find("void") == -1:
-            self.output += ": " + method["returns"]
-        self.output += "\n"
+            self.class_descriptions += ": " + method["returns"]
+        self.class_descriptions += "\n"
 
     def loadProperty(self, property, is_public: bool = True):
         if is_public:
-            self.output += "\t\t+" + property["name"] + "\n"
+            self.class_descriptions += "\t\t+" + property["name"] + "\n"
         else:
-            self.output += "\t\t-" + property["name"] + "\n"
+            self.class_descriptions += "\t\t-" + property["name"] + "\n"
+
+    def getOutput(self):
+        return "classDiagram\n"+self.class_hierarhy_str+self.class_descriptions
 
     pass
 
@@ -79,6 +83,6 @@ if __name__ == "__main__":
             sys.exit(1)
 
     parsed_file.generateClassDiagram()
-    print(parsed_file.output)
+    print(parsed_file.getOutput())
 
     pass
